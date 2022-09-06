b703bb5019c934e22f1cd78002de3995df5dc1b0
dannebrob <65211641+dannebrob@users.noreply.github.com>
2022-09-06 17:14:09 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import { useState } from "react";
 
 import bgCardFront from "./assets/images/bg-card-front.png";
 import bgCardBack from "./assets/images/bg-card-back.png";
-import bgMainDesktop from "./assets/images/bg-main-desktop.png";
-import bgMainMobile from "./assets/images/bg-main-mobile.png";
 import cardLogo from "./assets/images/card-logo.svg";
 import iconComplete from "./assets/images/icon-complete.svg";
 
@@ -19,7 +17,7 @@ function App() {
   const {
     register,
     handleSubmit,
-    watch,reset,
+    reset,
     formState: { errors, isSubmitSuccessful },
   } = useForm();
   const onSubmit = (data, e) => console.log(data, e)
@@ -37,43 +35,42 @@ function App() {
   }
   return (
     <div className="App" style={{ height: "100vh", width: "100vw" }}>
-      <div className="flex flex-wrap mobile:flex-col desktop:flex-row desktop:h-full justify-around w-full">
+      <div className="flex flex-wrap mobile:flex-col xl:flex-row xl:h-full w-full flex-wrap">
         <div
           id="container"
-          className="h-60 desktop:h-full  desktop:h-11/12 desktop:w-1/4 bg-cover bg-no-repeat flex justify-center aline-items"
-          style={{ backgroundImage: `url(${bgMainDesktop})` }}
+          className="h-60 w-full xl:h-11/12 xl:w-1/4  xl:h-full bg-cover bg-no-repeat flex justify-center items-center bg-mobile-bg sm:bg-desktop-bg "
         >
           <div className="relative h-[500px] w-[500px] m-auto">
           <div
             id="back-card"
-            className="w-80 desktop:  top-4 absolute left-20 rounded-md desktop:top-52"
+            className="w-80 top-4 absolute left-20 rounded-md xl:top-52"
           >
             <div id="img" className="absolute z-1 z-0">
-              <img src={bgCardBack} className="rounded-md " />
+              <img src={bgCardBack} className="rounded-md" alt="card background" />
             </div>
-            <p id="cvc" className="absolute top-[4.7rem] right-10 z-1 text-slate-50">
+            <p id="cvc" className="absolute top-[4.7rem] right-10 z-1 text-white">
               {cvc}
             </p>
           </div>
-          <div id="front-card" className="w-80 absolute left-8 top-28 m-auto desktop:top-0 desktop:left-10">
+          <div id="front-card" className="w-80 absolute left-8 top-28 m-auto xl:top-0 xl:left-10">
             <div id="img" className="absolute z-1 z-0">
-              <img src={bgCardFront} className="rounded-md" />
+              <img src={bgCardFront} className="rounded-md" alt="card background" />
             </div>
             <p
               id="numbers"
-              className="absolute top-20 left-4 z-1 text-slate-50 "
+              className="absolute top-20 left-4 z-1 text-white text-xl"
             >
               {injectSpaces(number)}
             </p>
-            <p id="name" className="absolute top-32 left-5 z-1">
+            <p id="name" className="absolute top-32 left-5 z-1 text-white">
               {name}
             </p>
-            <p id="until" className="absolute top-32 right-12 z-1">
+            <p id="until" className="absolute top-32 right-12 z-1 text-white">
               {expirationMonth} / {expirationYear}
             </p>
 
             <div id="icon" className="absolute top-[25px] left-4 z-1 w-12 h-12">
-              <img src={cardLogo} />
+              <img src={cardLogo} alt="card logo" />
             </div>
           </div>
           </div>
@@ -81,12 +78,11 @@ function App() {
          
         </div>
         {isSubmitSuccessful && (
-          <div className="w-11/12 h-full pt-14 z-1 desktop:w-2/4">
+          <div className="w-11/12 h-full pt-14 xl:py-40 z-1 xl:w-2/4">
             <div
-              className=" w-full h-7/12 mt-7/12 flex flex-col justify-center items-center "
-              style={{ backgroundColor: "white" }}
+              className=" w-full h-7/12 mt-7/12 flex flex-col justify-center items-center m-auto t-[50%] bg-white"
             >
-              <img src={iconComplete} className="w-24 pt-12" />
+              <img src={iconComplete} className="w-24 pt-12" alt="success icon" />
               <h1 className="text-4xl pt-10 tracking-wide">THANK YOU!</h1>
               <h2 className="text-xl pt-5 tracking-wide">
                 We added your card details
@@ -102,9 +98,9 @@ function App() {
           </div>
         )}
         {!isSubmitSuccessful && (
- <div id="form" className="w-11/12 h-full pt-20 z-1 desktop:w-3/4 flex justify-center m-auto desktop:items-center">
+ <div id="form" className="w-11/12 h-full pt-20 z-1 xl:w-3/4 flex justify-center m-auto xl:items-center">
  <form onSubmit={handleSubmit(onSubmit, onError)}>
-   <p className="uppercase text-sm text-textVeryDarkPurple font-bold">
+   <p className="uppercase text-sm text-very-dark-violet font-bold">
      cardholder name
    </p>
    <input
@@ -118,33 +114,34 @@ function App() {
      style={errors?.fullName ? { border: "3px solid red" } : null}
    />
    {errors.fullName && (
-     <p className="text-errorRed"> Can't be blank</p>
+     <p className="text-red"> Can't be blank</p>
    )}
-   <p className="uppercase text-sm text-textVeryDarkPurple font-bold mt-4">
+   <p className="uppercase text-sm text-very-dark-violet font-bold mt-4">
      Card number
    </p>
-   <input maxLength="16"
+   <input type="text" maxLength="16"
      {...register("number", { required: {value: true, message: 'Cant be blank'},   minLength: {
-       value: 12,
+       value: 16,
        message: "min length is 12"
-     },  maxLength: {
-       value: 12,
-       message: "max length is 12"
-     }})}
+     }, pattern: {
+      value: /^[0-9]*$/,
+      message: 'Wrong format, only numbers' // JS only: <p>error message</p> TS only support string
+    }
+    })}
      placeholder="000 000 000 000"
-     className="border-2 border-borderGray border-solid w-full h-10 p-2 rounded-md active:border-borderPurple "
+     className="border-2 border-borderGray border-solid w-full h-10 p-2 rounded-md active:border-active-gradient "
      onChange={(e) => {
        setNumber(e.target.value);
      }}
      style={errors?.number ? { border: "3px solid red" } : null}
    />
    {errors.number && (
-     <p className="text-errorRed">{errors.number.message}</p>
+     <p className="text-red">{errors.number.message}</p>
    )}
 
    <div className="flex flex-row w-full">
      <div className="w-1/2">
-       <p className="uppercase text-sm text-textVeryDarkPurple font-bold mt-4">
+       <p className="uppercase text-sm text-very-dark-violet font-bold mt-4">
          EXP. DATE (MM/YY)
        </p>
        <input
@@ -154,7 +151,7 @@ function App() {
            message: "min length is 2"
          }})}
          placeholder="MM"
-         className="border-2 border-borderGray border-solid h-10 p-2 rounded-md w-5/6 active:border-borderPurple"
+         className="border-2 border-borderGray border-solid h-10 p-2 rounded-md w-20 active:border-active-gradient"
          onChange={(e) => {
            setExpirationMonth(e.target.value);
          }}
@@ -167,35 +164,38 @@ function App() {
            message: "min length is 2"
          }})}
          placeholder="YY"
-         className="border-2 border-borderGray border-solid h-10 p-2 rounded-md w-5/6 active:border-borderPurple"
+         className="border-2 border-borderGray border-solid h-10 p-2 ml-2 rounded-md w-20 active:border-active-gradient"
          onChange={(e) => {
            setExpirationYear(e.target.value);
          }}
          style={errors?.year ? { border: "3px solid red" } : null}
        />
        {errors.month && (
-         <p className="text-errorRed">{errors.month.message}</p>
+         <p className="text-red">{errors.month.message}</p>
        )}
        {errors.year && (
-         <p className="text-errorRed">{errors.year.message}</p>
+         <p className="text-red">{errors.year.message}</p>
        )}
      </div>
      <div className="w-1/2">
-       <p className="uppercase text-sm text-textVeryDarkPurple font-bold mt-4">
+       <p className="uppercase text-sm text-very-dark-violet font-bold mt-4">
          CVC
        </p>
        <input
          type="number"
          maxLength="3"
-         {...register("cvc", { required: true })}
+         {...register("cvc", { required: {value: true, message: 'Cant be blank'}, minLength: {
+          value: 3,
+          message: "min length is 3"
+        }})}
          placeholder="e.g. 123"
-         className="border-2 border-borderGray border-solid h-10 p-2 rounded-md w-5/6 active:border-borderPurple"
+         className="border-2 border-borderGray border-solid h-10 p-2 rounded-md w-5/6 active:border-active-gradient"
          onChange={(e) => {
            setCvc(e.target.value);
          }}
          style={errors?.cvc ? { border: "3px solid red" } : null}
        />
-       {errors.cvc && <p className="text-errorRed"> Can't be blank</p>}
+       {errors.cvc && <p className="text-red">{errors.cvc.message}</p>}
      </div>
    </div>
    <div className="flex justify-center items-center">
@@ -211,6 +211,22 @@ function App() {
 </div>
         )}
        
+      </div>
+      <div className='mt-8 text-sm text-center'>
+        Challenge by{' '}
+        <a
+          className='text-[#610595]'
+          href='https://www.frontendmentor.io?ref=challenge'
+          target='_blank'
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by Daniel Brobäck
+        <a className='text-[#6348FE]' href='https://www.danielbroback.se'>
+          Q.A.
+        </a>
+        .
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,109 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    screens: {
-      mobile: "100px",
-      desktop: "1040px",
-    },
-    colors: {
-      borderGray: "hsl(270, 3%, 87%)",
-      borderPurple: "hsl(278, 94%, 30%)",
-      errorRed: "hsl(0, 100%, 66%)",
-      textVeryDarkPurple: "hsl(278, 68%, 11%)",
-    },
     extend: {
-      margin: {
-        320: "320px",
-      },
-      width: {
-        190: "190px",
-        275: "275px",
-        300: "300px",
-        340: "340px",
-        350: "350px",
-        656: "656px",
-        880: "880px",
-        508: "508px",
-      },
-      height: {
-        80: "80px",
-        340: "340px",
-        370: "370px",
-        420: "420px",
-        510: "510px",
-        600: "600px",
-        685: "685px",
-        800: "800px",
-        "90vh": "90vh",
-      },
-      flex: {
-        0.7: "0.7 1 0%",
-      },
-      maxHeight: {
-        370: "370px",
-      },
-      minWidth: {
-        210: "210px",
-        350: "350px",
-        620: "620px",
-      },
-      textColor: {
-        lightGray: "#F1EFEE",
-        primary: "#FAFAFA",
-        secColor: "#efefef",
-        navColor: "#BEBEBE",
-      },
-      backgroundColor: {
-        mainColor: "#FBF8F9",
-        secondaryColor: "#F0F0F0",
-        blackOverlay: "rgba(0, 0 ,0 ,0.7)",
-      },
-      backgroundImage: {
-      'bgMainMobile': "url('./assets/images/bg-main-mobile.png')",
-      'bgMainDesktop': "url('./assets/images/bg-main-desktop.png')",
-      },
-      keyframes: {
-        "slide-in": {
-          "0%": {
-            "-webkit-transform": "translateX(-200px)",
-            transform: "translateX(-200px)",
-          },
-          "100%": {
-            "-webkit-transform": "translateX(0px)",
-            transform: "translateX(0px)",
-          },
-        },
-
-        "slide-fwd": {
-          "0%": {
-            "-webkit-transform": "translateZ(0px)",
-            transform: "translateZ(0px)",
-          },
-          "100%": {
-            "-webkit-transform": "translateZ(160px)",
-            transform: "translateZ(160px)",
-          },
-        },
-      },
-      animation: {
-        "slide-in": "slide-in 0.5s ease-out",
-        "slide-fwd":
-          " slide-fwd 0.45s cubic-bezier(0.250, 0.460, 0.450, 0.940) both",
-      },
-      transitionProperty: {
-        height: "height",
+      colors: {
+        'active-gradient': 'hsl(249, 99%, 64%) to hsl(278, 94%, 30%)',
+        red: ' hsl(0, 100%, 66%)',
+        white: 'hsl(0, 0%, 100%)',
+        'light-grayish-violet': 'hsl(270, 3%, 87%)',
+        'dark-grayish-violet': 'hsl(279, 6%, 55%)',
+        'very-dark-violet': ' hsl(278, 68%, 11%)',
       },
     },
-    cursor: {
-      "zoom-in": "zoom-in",
-      pointer: "pointer",
+    backgroundImage: {
+      'mobile-bg': 'url("./assets/images/bg-main-mobile.png")',
+      'desktop-bg': 'url("./assets/images/bg-main-desktop.png")',
     },
-  },
-  variants: {
-    // backgroundColor: ['active'],
-    extend: {},
   },
   plugins: [],
 };
